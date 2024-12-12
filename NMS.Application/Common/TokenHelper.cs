a83d4bed9cd8780843d4fe2573afabdae91017c6
Showkat Akbar mithu <showkat131080@gmail.com>
2024-12-12 21:50:41 +0600

    Add project files.

---
+++ NMS.Application/Common/TokenHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMS.Application
+{
+    public static class TokenHelper
+    {
+        public const string issuer = "showkat.com";
+        public const string audience = "showkat.com";
+
+        public const string secret = "notemanagementsystemsecretnotemanagementsystemsecretnotemanagementsystemsecret";
+
+
+        public static string GenerateJwtToken(string username)
+        {
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Name, username),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
